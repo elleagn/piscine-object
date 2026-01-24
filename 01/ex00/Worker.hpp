edadd6761059716b7823e54fd24e2f62347b1cdb
Gaelle Ozon <gozon@student.42.fr>
2026-01-24 14:27:19 +0100

    Start worker header

---
+++ 01/ex00/Worker.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+struct Position
+{
+    int x;
+    int y;
+
+    Position();
+    ~Position();
+};
+
+struct Statistic {
+    int level;
+    int exp;
+    Statistic();
+    ~Statistic();
+};
+
+class Worker {
+    pri
+}

9d5e4a75512b0c7e249437c192bad137109095b6
Gaelle Ozon <gozon@student.42.fr>
2026-02-02 09:10:52 +0100

    Finish engine

--- 03/ex00/Engine.cpp
+++ 03/ex00/Engine.cpp
@@ -1,4 +1,5 @@
 #include "Engine.hpp"
+#include <iostream>
 
 Engine::Engine(): on(false) {}
 
@@ -11,4 +12,26 @@ Engine::~Engine() {}
 Engine& Engine::operator=(const Engine& assign) {
     on = assign.on;
     return (*this);
+}
+
+void Engine::start() {
+    if (on == true) {
+        std::cout << "Engine: Engine is already on." << std::endl;
+    } else {
+        on = true;
+        std::cout << "Engine: Engine started." << std::endl;
+    }
+}
+
+void Engine::stop() {
+    if (on == false) {
+        std::cout << "Engine: Engine is already off." << std::endl;
+    } else {
+        on = false;
+        std::cout << "Engine: Engine stopped." << std::endl;
+    }
+}
+
+void Engine::accelerate(int speed) {
+    std::cout << "Engine: Accelerated by " << speed << "km/h." << std::endl;
 }
